5949f59d534a95afd53ab97a6ec62ce73d1badec
A-Ahmad02 <134854740+A-Ahmad02@users.noreply.github.com>
2025-01-08 11:56:40 +0500

    Add files via upload

---
+++ TIVA side/mps_cep.d
@@ -0,0 +1,12 @@
+.\mps_cep.o: MPS_CEP.c
+.\mps_cep.o: C:\Program Files\ARM\Inc\TI\TM4C123\TM4C123.h
+.\mps_cep.o: C:\Program Files\ARM\Inc\TI\TM4C123\TM4C123GH6PM.h
+.\mps_cep.o: C:\Program Files\ARM\CMSIS\Include\core_cm4.h
+.\mps_cep.o: C:\Program Files\ARM\ARMCC\bin\..\include\stdint.h
+.\mps_cep.o: C:\Program Files\ARM\CMSIS\Include\core_cmInstr.h
+.\mps_cep.o: C:\Program Files\ARM\CMSIS\Include\core_cmFunc.h
+.\mps_cep.o: C:\Program Files\ARM\CMSIS\Include\core_cm4_simd.h
+.\mps_cep.o: C:\Program Files\ARM\Inc\TI\TM4C123\system_TM4C123.h
+.\mps_cep.o: address.h
+.\mps_cep.o: C:\Program Files\ARM\ARMCC\bin\..\include\stdio.h
+.\mps_cep.o: C:\Program Files\ARM\ARMCC\bin\..\include\stdlib.h
